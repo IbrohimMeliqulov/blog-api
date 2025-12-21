55f83b4ea68eb2daca13f5344684a197f47b0988
IbrohimMeliqulov <ibrohim.meliqulov2003@gmail.com>
2025-12-21 16:10:19 +0500

    validation added

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { postController } from "../controllers/post.controller.js";
+import { validateFunction } from "../middlewares/validate.middleware.js";
+import { PostSchema, PostUpdateSchema } from "../validation/post.validation.js";
 
 const postRouter = Router();
 postRouter.get("/", postController.findAll);
 postRouter.get("/search", postController.search.bind(postController));
 postRouter.get("/:id", postController.findOne);
-postRouter.post("/", postController.create);
-postRouter.put("/:id", postController.update);
+postRouter.post("/", validateFunction(PostSchema), postController.create);
+postRouter.put(
+  "/:id",
+  validateFunction(PostUpdateSchema),
+  postController.update,
+);
 postRouter.delete("/:id", postController.delete);
 
 export default postRouter;
---
+++ src/validation/post.validation.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+const PostSchema = z.object({
+  title: z.string(),
+  author_id: z.string(),
+  content: z.string().min(15).max(50),
+});
+
+const PostUpdateSchema = z.object({
+  title: z.string().optional(),
+  author_id: z.string().optional(),
+  content: z.string().min(15).max(50),
+});
+
+export { PostSchema, PostUpdateSchema };
